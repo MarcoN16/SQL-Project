f1a6e96590649bde964b77221c850f0c0db8a6fe
MarcoN16 <nigro.marco2@gmail.com>
2024-02-19 18:14:44 -0800

    updating files

--- EmployeeSQL/EmployeesSQL_analysis.sql
+++ EmployeeSQL/EmployeesSQL_analysis.sql
@@ -1,28 +1,30 @@
+
+
 -- List of employee
 SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
 FROM employees
-INNER JOIN salaries
+JOIN salaries
 ON employees.emp_no = salaries.emp_no;
 
  -- List of employee hired in 1986
 SELECT first_name, last_name, hire_date
 FROM employees
-WHERE EXTRACT(YEAR FROM hire_date) = 1986;
+WHERE hire_date LIKE '%1986';
 
 -- List of manager
 SELECT dept_manager.dept_no, departments.dept_name, dept_manager.emp_no, employees.last_name , employees.first_name
 FROM dept_manager
-INNER JOIN departments
+JOIN departments
 ON dept_manager.dept_no = departments.dept_no
-INNER JOIN employees
+JOIN employees
 ON dept_manager.emp_no = employees.emp_no;
 
 -- List of employees
 SELECT dept_emp.dept_no, employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
 FROM dept_emp
-INNER JOIN employees
+JOIN employees
 ON dept_emp.emp_no = employees.emp_no
-INNER JOIN departments
+JOIN departments
 ON dept_emp.dept_no = departments.dept_no;
 
 
@@ -45,9 +47,9 @@ WHERE emp_no IN (
 -- List each employee in the Sales and Development department
 SELECT employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
 FROM employees
-INNER JOIN dept_emp 
+JOIN dept_emp 
 ON employees.emp_no = dept_emp.emp_no
-INNER JOIN departments 
+JOIN departments 
 ON dept_emp.dept_no = departments.dept_no
 	WHERE departments.dept_no IN (
 		SELECT dept_no
@@ -59,7 +61,7 @@ ON dept_emp.dept_no = departments.dept_no
 SELECT last_name, COUNT(last_name) AS "counting"
 FROM employees
 GROUP BY last_name
-ORDER BY counting DESC
+ORDER BY "counting" DESC
 
 
 
